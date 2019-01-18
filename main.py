8470512edcce569a704053a2289062c50f06699f
Blessed <codingdreams4ever@gmail.com>
2019-01-18 14:08:10 -0600

    added form elements

---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+            
+        </style>
+    </head>
+    <body>
+        <form>
+            <label for = 'rotate-by'>Rotate By:</label>
+            <input id ='rotate-by' type = 'text' name='rot' placeholder ='0' />
+            <textarea type ='textarea' name = 'text'></textarea>
+            <input type ='submit' />
+            </form>
+    </body>
+</html>
+"""
+
+@app.route('/')
+def index():
+    return form
+
+app.run() 
